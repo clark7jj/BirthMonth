61efa183e0ec16e3a7c87c446fc376c9624d6b99
clark7jj <clark7jj@mail.uc.edu>
2026-09-25 13:22:44 -0400

    Completed Task #2

--- src/BirthMonth.java
+++ src/BirthMonth.java
@@ -1,13 +1,47 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class BirthMonth
+{
+    public static void main(String[] args)
+    {
+        // Pseudocode for Task #2
+        // class BirthMonth
+        //    main()
+        //        declare variables
+        //        num birthMonth = 0;
+        //        output "Enter your birth month in number: "
+        //        input birthMonth
+        //        Process
+        //        if birthMonth >= 1 and birthMonth <= 12 then
+        //            OUTPUT "Your birth month is: " + birthMonth
+        //        else
+        //            OUTPUT "You entered an incorrect month value: " + birthMonth
+        //        end if
+        //    return
+        // endClass
+
+        // Java Code for Task #2
+        {
+            Scanner in = new Scanner(System.in);
+            int birthMonth = 0;
+            String trash = "";
+
+            System.out.print("Enter your birth month in number: ");
+            if (in.hasNextInt()) {
+                birthMonth = in.nextInt();
+                in.nextLine(); // Clear the buffer
+            } else {
+                trash = in.nextLine(); // Clear the buffer
+                System.out.println("You entered an incorrect month value: " + trash);
+                System.exit(0);
+            }
+            // Process
+            if (birthMonth >= 1 && birthMonth <= 12) {
+                System.out.println("Your birth month is:  " + birthMonth);
+            } else {
+                System.out.println("You entered an incorrect month value:  " + birthMonth);
+        }
+        }
+
     }
 }
